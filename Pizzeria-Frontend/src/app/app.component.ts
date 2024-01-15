3a31b28cee117ec1d7c5ff77c96033f0f0d1a375
Jurayevkh <jurayevkh@gmail.com>
2024-01-15 23:42:50 +0500

    Added Carousel

--- Pizzeria-Frontend/src/app/app.component.ts
+++ Pizzeria-Frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NavbarComponent],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
