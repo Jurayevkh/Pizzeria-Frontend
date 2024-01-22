1aea2e3abb4ccaa029735167186411f0a26ed456
Jurayevkh <jurayevkh@gmail.com>
2024-01-22 05:15:15 +0500

    Declared

--- Pizzeria-Frontend/src/app/app.module.ts
+++ Pizzeria-Frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
    AppComponent,
    NavbarComponent,
    HomeComponent,
+   LoginComponent,
+   RegisterComponent,
   ],
   imports: [
     BrowserModule,
