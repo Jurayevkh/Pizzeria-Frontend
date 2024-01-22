ca790c997478d8181a4611c1837faaaf293dffe2
Jurayevkh <jurayevkh@gmail.com>
2024-01-22 05:15:08 +0500

    Routing

--- Pizzeria-Frontend/src/app/app-routing.module.ts
+++ Pizzeria-Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {path:"home",component:HomeComponent},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
   {path:"",redirectTo:"home",pathMatch:"full"}
 ];
 
