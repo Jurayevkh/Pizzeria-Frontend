316ca347826c9477af6a071b1b78ce9bb8bf27ba
Jurayevkh <jurayevkh@gmail.com>
2024-01-22 05:44:49 +0500

    Empty Basket component added

--- Pizzeria-Frontend/src/app/app-routing.module.ts
+++ Pizzeria-Frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { EmptyBasketComponent } from './pages/empty-basket/empty-basket.component';
 
 const routes: Routes = [
   {path:"home",component:HomeComponent},
   {path:"login",component:LoginComponent},
   {path:"register",component:RegisterComponent},
+  {path:"emptybasket",component:EmptyBasketComponent},
   {path:"",redirectTo:"home",pathMatch:"full"}
 ];
 
--- Pizzeria-Frontend/src/app/app.module.ts
+++ Pizzeria-Frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { EmptyBasketComponent } from './pages/empty-basket/empty-basket.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { RegisterComponent } from './pages/register/register.component';
    HomeComponent,
    LoginComponent,
    RegisterComponent,
+   EmptyBasketComponent,
   ],
   imports: [
     BrowserModule,
