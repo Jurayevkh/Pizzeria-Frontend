27797e25eab3d37a772c893446a16d49c52111fc
Jurayevkh <jurayevkh@gmail.com>
2024-01-22 05:14:46 +0500

    Login page finished

---
+++ Pizzeria-Frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
