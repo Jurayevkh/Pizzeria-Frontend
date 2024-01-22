751a987d7055e51daa55a552af1ecce89eb58740
Jurayevkh <jurayevkh@gmail.com>
2024-01-22 05:14:56 +0500

    Register pages finished

---
+++ Pizzeria-Frontend/src/app/pages/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+}
